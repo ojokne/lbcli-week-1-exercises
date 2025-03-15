5d7bf318ab3000eac2ca959d3f19252e7342cb69
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2025-03-15 14:36:31 +0300

    Revert changes made to setup.sh

--- .github/setup.sh
+++ .github/setup.sh
@@ -6,10 +6,7 @@ cat <<EOF > ~/.bitcoin/bitcoin.conf
 regtest=1
 rpcuser=user
 rpcpassword=password
-rpcport=18443
-rpcallowip=127.0.0.1
-server=1
-rpcbind=127.0.0.1
+rpcport=18332
 EOF
 
 echo $(bitcoin-cli --version)
